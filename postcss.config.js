3c5989e55d1ada2cc45eac37326024951db80803
PB <pablo.brady@gmail.com>
2019-09-04 12:28:04 -0700

    Add PurgeCSS for PostCSS

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,12 @@ module.exports = {
 	plugins: [
 		require('tailwindcss'),
 		require('autoprefixer'),
+		require('@fullhuman/postcss-purgecss')({
+			content: [
+				'./src/**/*.vue',
+				'./public/index.html'
+			],
+			defaultExtractor: content => content.match(/[A-Za-z0-9-_:/+]/g) || []
+		})
 	]
 }
