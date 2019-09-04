df73686eddbc78c90630be11b5e22bdc60817f04
PB <pablo.brady@gmail.com>
2019-09-04 12:43:05 -0700

    PurgeCSS on Production ENV only

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	plugins: [
 		require('tailwindcss'),
 		require('autoprefixer'),
-		require('@fullhuman/postcss-purgecss')({
+		process.env.NODE_ENV === 'production' && require('@fullhuman/postcss-purgecss')({
 			content: [
 				'./src/**/*.vue',
 				'./public/index.html'
